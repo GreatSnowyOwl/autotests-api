f886982f794acbcdd0bd900f10aa0eaf107026ce
Maksim Kamenskii <kamenskiy@simple.life>
2025-11-13 16:46:23 +0400

    Знакомство с Pydantic. Шаг 16

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,1 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, constr
+
+# Модель пользователя
+class UserSchema(BaseModel):
+    id: str 
+    email: EmailStr
+    last_name: constr(min_length=1, max_length=15)
+    first_name: constr(min_length=1, max_length=15)
+    middle_name: str
+
+# Модель запроса на создание пользователя
+class CreateUserRequestSchema(BaseModel):
+    email: EmailStr
+    password: constr(min_length=8)
+    last_name: constr(min_length=1, max_length=15)
+    first_name: constr(min_length=1, max_length=15)
+    middle_name: str
+
+# Модель ответа на создание пользователя
+class CreateUserResponseSchema(BaseModel):
+    user: UserSchema
